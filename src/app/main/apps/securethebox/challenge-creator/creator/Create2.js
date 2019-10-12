21568e8fafb7efe672947d6964d6706a2bf90482
Charles Chong <charleschong@Charless-MacBook-Pro.local>
2019-10-11 22:10:45 -0700

    Submit form challenge data to backend

--- src/app/main/apps/securethebox/challenge-creator/creator/Create2.js
+++ src/app/main/apps/securethebox/challenge-creator/creator/Create2.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import withReducer from 'app/store/withReducer';
 import connect from 'react-redux/es/connect/connect';
 import reducer from '../../../../../auth/store/reducers';
-import Styles from './Styles'
 import { Form, Field } from 'react-final-form'
 import ReactQuill from 'react-quill';
+import CourseFields from './CourseFields'
 import './Create.css'
 import arrayMutators from "final-form-arrays";
 import { FieldArray } from "react-final-form-arrays";
@@ -12,86 +12,18 @@ import {
     Paper,
     Grid,
     Button,
-    MenuItem,
 } from '@material-ui/core';
-import { TextField, Select } from 'final-form-material-ui';
+import CourseFieldsJson from './CourseFields.json'
+import CourseStepsJson from './CourseSteps.json'
+import axios from 'axios'
 
 class Create2 extends Component {
     constructor(props) {
         super(props);
         this.state = {
             loading: false,
-            course_fields: [
-                {
-                    "label": "Id",
-                    "name": "id",
-                    "component": "input",
-                    "type": "text",
-                    "placeholder": "1"
-                },
-                {
-                    "label": "Slug",
-                    "name": "slug",
-                    "component": "input",
-                    "type": "text",
-                    "placeholder": "incident-response-1"
-                },
-                {
-                    "label": "Title",
-                    "name": "title",
-                    "component": "textfull",
-                    "type": "text",
-                    "placeholder": "Incident Response 1"
-                },
-                {
-                    "label": "Description",
-                    "name": "description",
-                    "component": "textarea",
-                    "type": "text",
-                    "placeholder": "Responding to credential leak"
-                },
-                {
-                    "label": "Category",
-                    "name": "category",
-                    "component": "select",
-                    "placeholder": "Responding to credential leak"
-                }
-            ],
-            course_steps: [
-                {
-                    "id": 0,
-                    "title": "Overview",
-                    "content": "<p></p>"
-                },
-                {
-                    "id": 1,
-                    "title": "Grading Criteria",
-                    "content": "<p></p>"
-                },
-                {
-                    "id": 2,
-                    "title": "Scenario",
-                    "content": "<p></p>"
-                },
-                {
-                    "id": 3,
-                    "title": "Start Challenge",
-                    "content": "<p></p>"
-                }, {
-                    "id": 4,
-                    "title": "Resources",
-                    "content": "<p></p>"
-                }, {
-                    "id": 5,
-                    "title": "Submission",
-                    "content": "<p></p>"
-                },
-                {
-                    "id": 6,
-                    "title": "Scoring",
-                    "content": "<p></p>"
-                }
-            ]
+            course_fields: CourseFieldsJson,
+            course_steps: CourseStepsJson
         };
     }
 
@@ -117,63 +49,21 @@ class Create2 extends Component {
         )
     }
 
-    renderCourseFields() {
-        return (
-            this.state.course_fields.map((v) => {
-                if (v.component == "input") {
-                    return (
-                        <Grid key={v.label} item xs={2}>
-                            <Field
-                                name={v.name}
-                                label={v.label}
-                                component={TextField}
-                                type={v.type}
-                                required
-                                fullWidth
-                                placeholder={v.placeholder}
-                            />
-                        </Grid>
-                    )
-                } else if (v.component == "textfull") {
-                    return (
-                        <Grid key={v.label} item xs={12}>
-                            <Field
-                                name={v.name}
-                                label={v.label}
-                                required
-                                fullWidth
-                                component={TextField}
-                                type={v.type}
-                                placeholder={v.placeholder}
-                            />
-                        </Grid>
-                    )
-                } else if (v.component == "textarea") {
-                    return (
-                        <Grid key={v.label} item xs={12}>
-                            <Field
-                                name={v.name}
-                                label={v.label}
-                                component={TextField}
-                                type={v.type}
-                                multiline
-                                fullWidth
-                                placeholder={v.placeholder}
-                            />
-                        </Grid>
-                    )
-                }
+    submitChallengePayload() {
+        axios.post('/api/challenges', this.state.course_payload)
+            .then((r) => {
+                console.log(r)
             })
-        )
-
     }
 
     render() {
-        const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
         const onSubmit = async values => {
-            await sleep(300)
-            window.alert(JSON.stringify(values, 0, 2))
-            console.log(values)
+            let challengeDict = {}
+            challengeDict["challenge"] = values
+            axios.post('/api/challenges', challengeDict)
+                .then((r) => {
+                    console.log(r)
+                })
         }
 
         return (
@@ -186,7 +76,6 @@ class Create2 extends Component {
                         }}
                         initialValues={{
                             category: 'web',
-                            timer: 0,
                             length: 0,
                             totalSteps: 7,
                             activeStep: 0,
@@ -195,22 +84,7 @@ class Create2 extends Component {
                         render={({ handleSubmit, form: { mutators: { push, pop } }, form, submitting, pristine, values }) => (
                             <form onSubmit={handleSubmit}>
                                 <Grid container alignItems="flex-start" spacing={2}>
-                                    {this.renderCourseFields()}
-                                    <Grid item xs={2}>
-                                        <Field name="category" label="Select a Category" component={Select} fullWidth formControlProps={{ fullWidth: true }}>
-                                            <MenuItem value="web">Web</MenuItem>
-                                            <MenuItem value="incident_response">Incident Response</MenuItem>
-                                            <MenuItem value="threat_detection">Threat Detection</MenuItem>
-                                            <MenuItem value="vulnerability_management">Vulnerability Management</MenuItem>
-                                        </Field>
-                                    </Grid>
-                                    <Grid item xs={2}>
-                                        <Field name="timer" label="Select a Course Duration" component={Select} fullWidth formControlProps={{ fullWidth: true }}>
-                                            <MenuItem value={0}>No Timer</MenuItem>
-                                            <MenuItem value={30}>30 Minutes</MenuItem>
-                                            <MenuItem value={60}>1 Hour</MenuItem>>
-                                        </Field>
-                                    </Grid>
+                                    <CourseFields course_fields={this.state.course_fields} />
                                     <Grid item xs={12}>
                                         <Grid container alignItems="flex-start" spacing={2}>
                                             <FieldArray name="steps">
@@ -241,13 +115,10 @@ class Create2 extends Component {
                                             disabled={submitting || pristine}
                                         >
                                             Reset
-                                    </Button>
+                                        </Button>
                                         <Button type="submit" variant="contained" color="primary" disabled={submitting || pristine}>
                                             Submit
-                                    </Button>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <pre>{JSON.stringify(values, 0, 2)}</pre>
+                                        </Button>
                                     </Grid>
                                 </Grid>
 
